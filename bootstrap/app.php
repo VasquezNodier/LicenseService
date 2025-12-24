d93cf4eeca3879017fc64dcaa96d4d58852d67ae
Nodier Vasquez <vasqueznap@gmail.com>
2025-12-23 20:05:04 -0500

    config: register middlewares

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->alias([
+            'brand.auth' => \App\Http\Middleware\BrandAuth::class,
+            'product.auth' => \App\Http\Middleware\ProductAuth::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
