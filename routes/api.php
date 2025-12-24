1ab0897beefd5600ff4829264fe3a486400ba15b
Nodier Vasquez <vasqueznap@gmail.com>
2025-12-24 13:04:09 -0500

    del: user endpoint

--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,6 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
 Route::get('/health', fn () => response()->json(['status' => 'ok']));
 
 Route::get('/ready', function () {
