578d9bc0dc08d59a9967c15317582801f6ee4b7d
Nodier Vasquez <vasqueznap@gmail.com>
2025-12-24 00:41:20 -0500

    add: routes for us1, us3, us,4 and us6

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::prefix('brand')->middleware('brand.auth')->group(function () {
+    Route::post('/license-keys', \App\Http\Controllers\Api\Brand\ProvisionLicenseKeyController::class);
+    Route::get('/licenses', \App\Http\Controllers\Api\Brand\ListLicensesByEmailController::class);
+ });
+
+Route::prefix('product')->middleware('product.auth')->group(function () {
+    Route::post('/activate', \App\Http\Controllers\Api\Product\ActivateLicenseController::class);
+    Route::get('/license-keys/{key}', \App\Http\Controllers\Api\Product\LicenseKeyStatusController::class);
+});
